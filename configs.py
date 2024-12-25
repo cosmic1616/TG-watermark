ca2be9ca5f63e5e3cd037ae56f786d24c9cde2e7
cosmic1616 <cosmiccrazy16@gmail.com>
2024-12-25 12:37:05 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -5,10 +5,10 @@ load_dotenv()
 
 class Config:
     BOT_TOKEN = os.getenv("BOT_TOKEN")
-    API_ID = os.getenv("API_ID")
-    API_HASH = os.getenv("API_HASH")
-    LOG_CHANNEL = os.getenv("LOG_CHANNEL")
-    UPDATES_CHANNEL = os.getenv("UPDATES_CHANNEL")
-    OWNER_ID = int(os.getenv("OWNER_ID"))
+    API_ID = os.getenv("API_ID","23630387")
+    API_HASH = os.getenv("API_HASH","3139998ebc89c2bc1873b794689f8e56")
+    LOG_CHANNEL = os.getenv("LOG_CHANNEL","-1002396213226")
+    UPDATES_CHANNEL = os.getenv("UPDATES_CHANNEL","-1002396213226")
+    OWNER_ID = int(os.getenv("OWNER_ID","7590766084"))
     DOWN_PATH = "downloads"
     PRESET = "ultrafast"
